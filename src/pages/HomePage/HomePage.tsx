697abf7f06fea12e692e7317dececb70d372e764
Artem Sydun <sydun2003@gmail.com>
2024-01-22 14:18:42 +0200

    list fixes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,45 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Product } from '../../types/product';
+import { Loader } from '../../components/Loader';
 
 export const HomePage = () => {
-  const [productList, setProductList] = useState('');
+  const [productList, setProductList] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://gadjets-store.onrender.com/products')
-      .then((res) => res.json())
-      .then(setProductList);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3005/products');
+
+        setProductList(response.data);
+      } catch (error) {
+        throw new Error('error');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div className="container">
-      {productList}
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ul>
+          {productList.map((product) => {
+            const { id, image } = product;
+
+            return (
+              <li key={id}>
+                <img src={`${image}`} alt="" />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
