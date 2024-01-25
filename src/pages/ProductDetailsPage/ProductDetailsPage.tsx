0ace3f2d8aa52533f5013f50a7a97c002a7d9c7d
Emiliia Kaminska <milja2705@gmail.com>
2024-01-25 13:12:05 +0200

    [UI] fixed page grid in the detailsProductPage

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -7,9 +7,6 @@ import {
 } from '../../components/AboutProduct/AboutProduct';
 import { TechSpecs } from '../../components/TechSpecs/TechSpecs';
 import { ProductImagesSlider } from '../../components/ProductImagesSlider';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
 
 export const ProductDetailsPage = () => {
   return (
@@ -17,23 +14,29 @@ export const ProductDetailsPage = () => {
       <div className={detailsStyles.container}>
         <ProductTitle />
         <div className={detailsStyles.extendedDetails}>
-          <div className={detailsStyles.extendedDetails__pictures}>
-            <ProductImagesSlider />
+          <div className={detailsStyles.topContent}>
+            <div className={detailsStyles.extendedDetails__pictures}>
+              <ProductImagesSlider />
+            </div>
+            <div className={detailsStyles.extendedDetails__mainInfo}>
+              mainInfo
+            </div>
           </div>
-          <div className={detailsStyles.extendedDetails__mainInfo}>
-            mainInfo
+
+          <div className={detailsStyles.bottomContent}>
+            <div className={detailsStyles.extendedDetails__about}>
+              <AboutProduct />
+            </div>
+            <div className={detailsStyles.extendedDetails__techSpecs}>
+              {phonesFromServer.map(phone => (
+                <TechSpecs
+                  phone={phone}
+                  key={phone.id}
+                />
+              ))}
+            </div>
           </div>
-          <div className={detailsStyles.extendedDetails__about}>
-            <AboutProduct />
-          </div>
-          <div className={detailsStyles.extendedDetails__techSpecs}>
-            {phonesFromServer.map(phone => (
-              <TechSpecs
-                phone={phone}
-                key={phone.id}
-              />
-            ))}
-          </div>
+
         </div>
 
         <h1>You may also like</h1>
