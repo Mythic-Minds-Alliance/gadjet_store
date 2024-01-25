def9af309c6814c9c8d1efe0577c92cbea47da2a
ArtemSydun <71509028+ArtemSydun@users.noreply.github.com>
2024-01-25 16:23:39 +0200

    Product tech specs under button (not fixed yet) (#72)
    * product controllers price and add button implemented
    
    * controllers tech specs (not fixed)

--- src/components/DetailsList/DetailsList.tsx
+++ src/components/DetailsList/DetailsList.tsx
@@ -2,15 +2,15 @@ import { Product } from '../../types/product';
 import './DetailsList.scss';
 
 type Props = {
-  product: Product,
+  product?: Product,
 };
 
 export const DetailsList: React.FC<Props> = ({ product }) => {
-  const {
-    screen,
-    capacity,
-    ram,
-  } = product;
+  // const {
+  //   screen,
+  //   capacity,
+  //   ram,
+  // } = product;
 
   return (
     <div className="details">
@@ -19,12 +19,20 @@ export const DetailsList: React.FC<Props> = ({ product }) => {
         <li className="details--list-item">Capacity</li>
         <li className="details--list-item">RAM</li>
       </ul>
+      {(product ? (
+        <ul className="details--characteristics">
+          <li className="details--characteristics-item">{product.screen}</li>
+          <li className="details--characteristics-item">{product.capacity}</li>
+          <li className="details--characteristics-item">{product.ram}</li>
+        </ul>
+      ) : (
+        <ul className="details--characteristics">
+          <li className="details--characteristics-item">screen</li>
+          <li className="details--characteristics-item">capacity</li>
+          <li className="details--characteristics-item">ram</li>
+        </ul>
+      ))}
 
-      <ul className="details--characteristics">
-        <li className="details--characteristics-item">{screen}</li>
-        <li className="details--characteristics-item">{capacity}</li>
-        <li className="details--characteristics-item">{ram}</li>
-      </ul>
     </div>
   );
 };
