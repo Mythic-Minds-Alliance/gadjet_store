4f50ec804910dad44ceae2c5d9b2b1c905d2d26a
Emiliia Kaminska <milja2705@gmail.com>
2024-01-26 16:39:06 +0200

    [UI] made capitalized crumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -25,7 +25,12 @@ export const Breadcrumbs = () => {
       return (
         <React.Fragment key={crumb}>
           <div className={crumbStyle}>
-            <Link to={currentLink}>{crumb.replaceAll('%20', ' ')}</Link>
+            <Link
+              to={currentLink}
+              className={style.crumb}
+            >
+              {crumb.replaceAll('%20', ' ')}
+            </Link>
           </div>
           {!isLastCrumb && (
             <img
