4f6661cf9fcd8f73dfba20ce7ce46639c41ef10c
Emiliia Kaminska <75914714+emilionila@users.noreply.github.com>
2024-01-26 16:34:00 +0200

    * [UI] created colors and capacity blocks
    * [UI] created colors and capacity blocks
    
    ---------
    
    Co-authored-by: ArtemSydun <71509028+ArtemSydun@users.noreply.github.com>

---
+++ src/components/CardCapacity/CardCapacity.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import style from './CardCapacity.module.scss';
+
+type Props = {
+  capacities: number[],
+};
+export const CardCapacity: React.FC<Props> = ({ capacities }) => {
+  return (
+    <div className={style.CardCapacity}>
+      <p className={style.CardCapacity__title}>
+        Select capacity
+      </p>
+
+      <div className={style.CardCapacity__list}>
+        {capacities.map(capacity => (
+          <div className={style.CardCapacity__item}>
+            {capacity}
+            {' '}
+            GB
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
