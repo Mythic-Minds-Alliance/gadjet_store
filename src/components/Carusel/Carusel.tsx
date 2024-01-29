86efa91a949e9cab25ac00ec6533903bb5573a5f
Maria Berezhna <marua545@gmail.com>
2024-01-29 13:23:00 +0200

    l

--- src/components/Carusel/Carusel.tsx
+++ src/components/Carusel/Carusel.tsx
@@ -1,14 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import classNames from 'classnames';
 import './Carusel.scss';
 import { DataContext } from '../../App';
 import { sortProductCarusel } from '../../utils/helpers';
 import { Card } from '../Card/Card';
 import arrow from '../../icons/SliderButtonRight.png';
-
-const CART_W = 229;
-const GAP = 16;
-const CARUSEL_STEP = CART_W + GAP;
 
 interface Props {
   title: string;
@@ -21,13 +17,6 @@ export const Carusel: React.FC<Props> = ({
 }) => {
   const { productList } = useContext(DataContext);
   const visibleCart = sortProductCarusel(productList, selectedSortCarusel);
-  const [wCounter, setWCounter] = useState(0);
-
-  const SLIDER_W = visibleCart.length * CARUSEL_STEP;
-
-  const handleSlide = (step: number) => {
-    setWCounter(wCounter + step);
-  };
 
   return (
     <section className="carusel">
@@ -38,12 +27,8 @@ export const Carusel: React.FC<Props> = ({
             className={classNames(
               'button',
               'button--left',
-              { 'button--disabled': wCounter === 0 },
             )}
             type="button"
-            onClick={() => {
-              handleSlide(-CART_W);
-            }}
           >
             <img
               src={arrow}
@@ -57,12 +42,8 @@ export const Carusel: React.FC<Props> = ({
             className={classNames(
               'Button',
               'Button--right',
-              { 'button--disabled': wCounter >= SLIDER_W - CARUSEL_STEP },
             )}
             aria-label="Go right"
-            onClick={() => {
-              handleSlide(CART_W);
-            }}
           >
             <img
               src={arrow}
@@ -74,19 +55,11 @@ export const Carusel: React.FC<Props> = ({
       </div>
 
       <div className="carusel__slider">
-        <div
-          className="carusel__sliderFull"
-          style={{
-            transform: `translateX(${-wCounter}px)`,
-            transition: 'transform 0.5s ease-in-out',
-          }}
-        >
-          {visibleCart.map((product) => (
-            <div className="caruselSlide" key={product.id}>
-              <Card product={product} />
-            </div>
-          ))}
-        </div>
+        {visibleCart.map((product) => (
+          <div className="caruselSlide" key={product.id}>
+            <Card product={product} />
+          </div>
+        ))}
       </div>
     </section>
   );
