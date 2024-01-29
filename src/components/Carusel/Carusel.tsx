1c755a9d6b17037a4600b97680f2d69dc0bc0130
Maria Berezhna <marua545@gmail.com>
2024-01-29 13:55:50 +0200

    l

--- src/components/Carusel/Carusel.tsx
+++ src/components/Carusel/Carusel.tsx
@@ -1,10 +1,96 @@
 import React, { useContext } from 'react';
 import classNames from 'classnames';
-import './Carusel.scss';
+import Carousel, { ButtonGroupProps } from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 import { DataContext } from '../../App';
 import { sortProductCarusel } from '../../utils/helpers';
 import { Card } from '../Card/Card';
 import arrow from '../../icons/SliderButtonRight.png';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1200 },
+    items: 4,
+    slidesToSlide: 2,
+  },
+  tablet: {
+    breakpoint: { max: 1199, min: 860 },
+    items: 3,
+    slidesToSlide: 1,
+    partialVisibilityGutter: 30,
+  },
+  tabletSmall: {
+    breakpoint: { max: 859, min: 641 },
+    items: 2,
+    slidesToSlide: 1,
+    partialVisibilityGutter: 60,
+  },
+  largeMobile: {
+    breakpoint: { max: 640, min: 400 },
+    items: 1.5,
+    slidesToSlide: 1,
+  },
+  mobile: {
+    breakpoint: { max: 400, min: 320 },
+    items: 1.1,
+    slidesToSlide: 1,
+  },
+};
+
+const ButtonGroup: React.FC = ({
+  next,
+  previous,
+  carouselState,
+}: ButtonGroupProps) => {
+  const currentSlide = carouselState?.currentSlide ?? 0;
+  const isInitialSlide = currentSlide === 0;
+
+  const handlePrevClick = () => {
+    if (previous) {
+      previous();
+    }
+  };
+
+  const handleNextClick = () => {
+    if (next) {
+      next();
+    }
+  };
+
+  return (
+    <div className="carousel-button-group">
+      <button
+        type="button"
+        className={classNames(
+          'button',
+          { 'button--disabled': isInitialSlide },
+        )}
+        aria-label="Go left"
+        onClick={handlePrevClick}
+      >
+        <img
+          src={arrow}
+          alt="arrow_left"
+          className="button__img"
+        />
+      </button>
+
+      <button
+        type="button"
+        className="Button Button--right"
+        aria-label="Go right"
+        onClick={handleNextClick}
+      >
+        <img
+          src={arrow}
+          alt="arrou_right"
+          className="button__img button--right"
+        />
+      </button>
+    </div>
+  );
+};
 
 interface Props {
   title: string;
@@ -15,51 +101,27 @@ export const Carusel: React.FC<Props> = ({
   title,
   selectedSortCarusel,
 }) => {
-  const { productList } = useContext(DataContext);
-  const visibleCart = sortProductCarusel(productList, selectedSortCarusel);
+  const { cartStorage } = useContext(DataContext);
+  const visibleCart = sortProductCarusel(cartStorage, selectedSortCarusel);
 
   return (
     <section className="carusel">
-      <div className="carusel__header">
-        <div className="carusel__title">{title}</div>
-        <div className="carousel-buttons">
-          <button
-            className={classNames(
-              'button',
-              'button--left',
-            )}
-            type="button"
-          >
-            <img
-              src={arrow}
-              alt="arrow_left"
-              className="button__img"
-            />
-          </button>
-
-          <button
-            type="button"
-            className={classNames(
-              'Button',
-              'Button--right',
-            )}
-            aria-label="Go right"
-          >
-            <img
-              src={arrow}
-              alt="arrou_right"
-              className="button__img button--right"
-            />
-          </button>
-        </div>
-      </div>
+      <div className="carusel__title">{title}</div>
 
       <div className="carusel__slider">
-        {visibleCart.map((product) => (
-          <div className="caruselSlide" key={product.id}>
-            <Card product={product} />
-          </div>
-        ))}
+        <Carousel
+          itemClass="Cards"
+          arrows={false}
+          renderButtonGroupOutside
+          customButtonGroup={<ButtonGroup />}
+          partialVisible
+          responsive={responsive}
+          infinite
+        >
+          {visibleCart.map((product) => (
+            <Card product={product} key={product.id} />
+          ))}
+        </Carousel>
       </div>
     </section>
   );
