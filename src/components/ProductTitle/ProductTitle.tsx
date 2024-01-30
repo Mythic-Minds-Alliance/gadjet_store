ea27d236c13fb1929253b34357e6ba1594f31305
Artem Sydun <sydun2003@gmail.com>
2024-01-30 16:43:55 +0200

    data from link

--- src/components/ProductTitle/ProductTitle.tsx
+++ src/components/ProductTitle/ProductTitle.tsx
@@ -1,13 +1,14 @@
+import { Product } from '../../types/product';
 import productTitleStyles from './ProductTitle.module.scss';
 
 type Props = {
-  title: string;
+  title: Product;
 };
 
 export const ProductTitle: React.FC<Props> = ({ title }) => {
   return (
     <h1 className={productTitleStyles.title}>
-      {title}
+      {title.name}
     </h1>
   );
 };
