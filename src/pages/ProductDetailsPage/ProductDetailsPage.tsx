81a7de0c5c68cb2bed1428956180699fd46d30f9
Artem Sydun <sydun2003@gmail.com>
2024-01-30 15:55:13 +0200

    card links

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable max-len */
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 import detailsStyles from './ProductDetailsPage.module.scss';
 import { ProductTitle } from '../../components/ProductTitle';
 import {
@@ -15,8 +18,37 @@ import { Carusel } from '../../components/Carusel';
 import { CaruselSort } from '../../types/CaruselSort';
 import { BackButton } from '../../components/BackButton';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { Product } from '../../types/product';
 
 export const ProductDetailsPage = () => {
+  const [, setCurrentProduct] = useState<Product>();
+  const [, setIsLoading] = useState(true);
+
+  const queris = useLocation();
+  const searchParams = new URLSearchParams(queris.search);
+  const capacity = searchParams.get('capacity');
+  const productId = searchParams.get('productId');
+  const color = searchParams.get('color');
+
+  const requestedPhone = `http://localhost:3005/products?productId=${productId}&color=${color}&capacity=${capacity}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios
+          .get(requestedPhone);
+
+        setCurrentProduct(response.data);
+      } catch (error) {
+        throw new Error('error when fetching data from API');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [requestedPhone]);
+
   return (
     <>
       <div className={detailsStyles.container}>
