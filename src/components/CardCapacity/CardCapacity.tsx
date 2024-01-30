58483e40f75a1faaa3f5d8c3e6f0a4b4674dbe0c
Malyholovka Ostap <maligolovkaostap@gmail.com>
2024-01-30 19:42:29 +0200

    added fucn for capacity

--- src/components/CardCapacity/CardCapacity.tsx
+++ src/components/CardCapacity/CardCapacity.tsx
@@ -13,13 +13,26 @@ export const CardCapacity: React.FC<Props> = ({ capacities }) => {
 
       <div className={style.CardCapacity__list}>
         {capacities
-          .map(capacity => parseFloat(capacity))
+          .map(capacity => {
+            const value = parseFloat(capacity);
+
+            if (capacity.includes('TB')) {
+              return value * 1024;
+            }
+
+            return value;
+          })
           .sort((a, b) => a - b)
-          .map(capacity => (
-            <div key={capacity} className={style.CardCapacity__item}>
-              {`${capacity}GB`}
-            </div>
-          ))}
+          .map(capacity => {
+            const value = capacity < 1024 ? capacity : capacity / 1024;
+            const unit = capacity < 1024 ? 'GB' : 'TB';
+
+            return (
+              <div key={capacity} className={style.CardCapacity__item}>
+                {`${value}${unit}`}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
