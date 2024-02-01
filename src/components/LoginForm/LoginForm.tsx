e8e6559c546464ed191b981b3b700280023e7055
Emiliia Kaminska <75914714+emilionila@users.noreply.github.com>
2024-02-01 11:39:33 +0200

    * [UI] added link to form on burger
    * added scroll to top for cart & favorite
    
    * added search
    
    * added func for search
    
    * added search for all product
    
    * added style for search btn
    
    * fixed empty specs
    
    * added trim for query
    
    * added maping for capacity
    
    * [BUGS] fixed cart
    
    * [API] mapped colors
    
    * [UI] added image change on colors logic
    
    * [BUGS] fixed carusel
    
    * [BUGS] fixed links in footer and shop by category
    
    * [BUGS] added active on color and capacity
    
    * [UI] added state for colors and capacity
    
    * [UI] added colors for products
    
    * [UI] created registration page
    
    * [UI] added registartion form
    
    * [UI] finished registration form
    
    * [UI] added login component
    
    * [UI] added login form
    
    * [UI] added scroll to top
    
    * [UI] added link to form on burger
    
    ---------
    
    Co-authored-by: Malyholovka Ostap <maligolovkaostap@gmail.com>

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom';
+import style from './LoginForm.module.scss';
+import { scrollToTop } from '../../utils/helpers';
+
+export const LoginForm = () => {
+  return (
+    <form
+      action="#"
+      method="post"
+      className={style.LoginForm}
+    >
+      <h2 className={style.LoginForm__title}>Log In</h2>
+
+      <div className={style.LoginForm__item}>
+        <label
+          htmlFor="email"
+          className={style.LoginForm__label}
+        >
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+          className={style.LoginForm__input}
+        />
+      </div>
+
+      <div className={style.LoginForm__item}>
+
+        <label
+          htmlFor="password"
+          className={style.LoginForm__label}
+        >
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+          className={style.LoginForm__input}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className={style.LoginForm__submitBtn}
+      >
+        Log In
+      </button>
+
+      <p className={style.LoginForm__haveAnAcc}>
+        Do not have an account yet?
+        <Link
+          to="/account/register"
+          className={style.LoginForm__register}
+          onClick={scrollToTop}
+        >
+          Register
+        </Link>
+      </p>
+    </form>
+  );
+};
