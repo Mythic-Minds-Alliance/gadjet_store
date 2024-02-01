ac78e9ca93c1fcdeecd16a785e015dc155fc9189
Emiliia Kaminska <milja2705@gmail.com>
2024-02-01 19:22:18 +0200

    [UI] changed breadcrumbs logic

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {
+  useEffect, useState, ReactNode, useRef,
+} from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import HomeIcon from '../../icons/Home.svg';
 import style from './Breadcrumbs.module.scss';
@@ -6,55 +8,59 @@ import arrow from '../../icons/Arrow.svg';
 
 export const Breadcrumbs = () => {
   const location = useLocation();
+  const [crumbs, setCrumbs] = useState<ReactNode>([]);
+  const isHomePage = location.pathname === '/';
+  const currentLinkRef = useRef<string>(''); // Use a useRef for mutable value
 
-  let currentLink = '';
+  useEffect(() => {
+    if (!isHomePage) {
+      const pathArray = location.pathname
+        .replaceAll('%20', ' ')
+        .split('/')
+        .filter(crumb => crumb !== '');
 
-  const crumbs = location.pathname.split('/')
-    .filter(crumb => crumb !== '')
-    .map((crumb, index, array) => {
-      currentLink += `/${crumb}`;
+      const breadcrumbs = pathArray.map((crumb, index, array) => {
+        currentLinkRef.current += `/${crumb}`;
 
-      const isLastCrumb = index === array.length - 1;
-      const crumbStyle = isLastCrumb ? style.lastCrumb : style.crumb;
+        const isLastCrumb = index === array.length - 1;
+        const crumbStyle = isLastCrumb ? style.lastCrumb : style.crumb;
 
-      return (
-        <React.Fragment key={crumb}>
-          <div className={crumbStyle}>
-            <Link
-              to={currentLink}
-              className={style.crumb}
-            >
-              {crumb.replaceAll('%20', ' ')}
-            </Link>
-          </div>
-          {!isLastCrumb && (
-            <img
-              src={arrow}
-              alt="arrow right"
-              className={style.arrowRight}
-            />
-          )}
-        </React.Fragment>
-      );
-    });
+        const displayCrumb
+                    = crumb.replaceAll('%20', ' ')
+                      .split(' ').length >= 3
+                      ? crumb.replaceAll('%20', ' ')
+                        .split(' ').slice(0, -2).join(' ')
+                      : crumb.replaceAll('%20', ' ');
+
+        return (
+          <React.Fragment key={crumb}>
+            <div className={crumbStyle}>
+              <Link to={currentLinkRef.current} className={style.crumb}>
+                {displayCrumb}
+              </Link>
+            </div>
+            {!isLastCrumb && (
+              <img src={arrow} alt="arrow right" className={style.arrowRight} />
+            )}
+          </React.Fragment>
+        );
+      });
+
+      setCrumbs(breadcrumbs);
+    }
+  }, [location, isHomePage]);
 
   return (
     <div className={style.breadcrumbs}>
       <Link to="/">
-        <img
-          src={HomeIcon}
-          alt="icon home"
-          className={style.homeImg}
-        />
+        <img src={HomeIcon} alt="icon home" className={style.homeImg} />
       </Link>
-      <img
-        src={arrow}
-        alt="arrow right"
-        className={style.arrowRight}
-      />
-      <div className={style.crumbsContainer}>
-        {crumbs}
-      </div>
+      {!isHomePage && (
+        <>
+          <img src={arrow} alt="arrow right" className={style.arrowRight} />
+          <div className={style.crumbsContainer}>{crumbs}</div>
+        </>
+      )}
     </div>
   );
 };
